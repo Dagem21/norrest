625d507158e1251784cd355ba0925f738acdc5e5
Dagem Moges Gizaw <DagemMoges@cbe.com.et>
2026-06-11 15:33:42 +0300

    menu

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -4,6 +4,7 @@ import MenuItem from "@/components/menuItem";
 import { MenuContext } from "@/providers/menu";
 import { useContext, useEffect } from "react";
 import profile from "../../assets/images/radblu.jpg";
+import Image from "next/image";
 
 export default function Branch() {
     const menuContext = useContext(MenuContext);
@@ -17,8 +18,16 @@ export default function Branch() {
             <div className="flex flex-col w-full">
                 <div className="flex flex-wrap justify-center gap-2 w-full">
                     <div className="relative flex flex-col gap-2 w-screen h-fit sm:w-4/7">
+                        <div className="bg-taupe-200 dark:bg-taupe-600 p-2 rounded-lg flex flex-col items-center">
+                            <Image
+                                className="w-15 h-15 object-cover"
+                                src={profile}
+                                alt={"Company profile"}
+                            />
+                            <h1 className="text-md font-bold">{"Company Name"}</h1>
+                        </div>
                         <div className="p-2 bg-taupe-200 dark:bg-taupe-600 rounded-lg">
-                            <h1 className="text-sm font-bold text-center text-taupe-600 dark:text-taupe-200">
+                            <h1 className="text-sm font-semibold text-center text-taupe-600 dark:text-taupe-200">
                                 Menu
                             </h1>
                             <hr className="m-3 border-taupe-500 dark:border-taupe-400" />
@@ -27,12 +36,22 @@ export default function Branch() {
                                     No items in menu.
                                 </h1>
                             </div> */}
-                            <MenuItem
-                                price={500.0}
-                                name="Pizza"
-                                categories="Breakfast"
-                                image={profile}
-                            />
+                            <div className="flex flex-col gap-2">
+                                <MenuItem
+                                    price={500.0}
+                                    name="Pizza"
+                                    categories="Breakfast"
+                                    description="breakfast food"
+                                    image={profile}
+                                />
+                                <MenuItem
+                                    price={500.0}
+                                    name="Pizza"
+                                    categories="Breakfast"
+                                    image={profile}
+                                    discount={0.1}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
--- components/menuItem.tsx
+++ components/menuItem.tsx
@@ -4,23 +4,42 @@ type MenuItemProps = {
     name: string;
     price: number;
     image: StaticImageData;
+    description?: string;
     categories: string;
+    discount?: number;
 };
 
-export default function MenuItem({ name, price, image, categories }: MenuItemProps) {
+export default function MenuItem({
+    name,
+    price,
+    image,
+    description,
+    categories,
+    discount,
+}: MenuItemProps) {
     return (
-        <div className="flex items-center justify-between px-2">
-            <div className="flex">
+        <div className="flex items-center justify-between p-2 shadow-lg text">
+            <div className="flex items-center">
                 <div>
                     <Image className="w-15 h-15 rounded-lg object-cover" src={image} alt={name} />
                 </div>
-                <div className="mx-2">
-                    <h1 className="text-lg font-bold">{name}</h1>
-                    <p className="text-sm">{categories}</p>
+                <div className="mx-2 tracking-wide">
+                    <h1 className="text-md font-bold">{name}</h1>
+                    <p className="text-xs">{description}</p>
+                    <p className="text-xs">{categories}</p>
                 </div>
             </div>
             <div>
-                <span>${price}</span>
+                <span
+                    className={`flex flex-col ${discount ? "text line-through text-xs text-taupe-400" : "font-bold text-sm text-taupe-200"}`}
+                >
+                    $ {price} Birr
+                </span>
+                {discount && (
+                    <span className="font-bold text-sm text-taupe-200">
+                        $ {price - price * discount} Birr
+                    </span>
+                )}
             </div>
         </div>
     );
